7944c05ee6c81a289ff2d5aebf426e1fde74287e
murali <murali@rovarinfotech.com>
2020-06-27 20:11:32 -0400

    Add project files.

---
+++ Controllers/CalendarController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Trial2.Models;
+
+namespace Trial2.Controllers
+{
+    public class CalendarController : Controller
+    {
+        private CalendarDataEntities db = new CalendarDataEntities();
+
+        // GET: Calendar
+        public ActionResult Calendar(DateTime? appointmentDate)
+        {
+            ViewBag.DateTimeScheduled = appointmentDate ?? DateTime.Now;
+            return View();
+        }
+
+        // GET: Calendar/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table table = db.Tables.Find(id);
+            if (table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table);
+        }
+
+        // GET: Calendar/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Calendar/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Title,DateTimeScheduled,Category,Status")] Table table)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tables.Add(table);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(table);
+        }
+
+        // GET: Calendar/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table table = db.Tables.Find(id);
+            if (table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table);
+        }
+
+        // POST: Calendar/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Title,DateTimeScheduled,Category,Status")] Table table)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(table).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(table);
+        }
+
+        // GET: Calendar/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table table = db.Tables.Find(id);
+            if (table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table);
+        }
+
+        // POST: Calendar/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Table table = db.Tables.Find(id);
+            db.Tables.Remove(table);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
